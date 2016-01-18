7064065f5d52809b1cf4856e6962d76abf57c5e6
Dafin <dafin@users.noreply.github.com>
2016-01-18 09:52:25 +0000

    Added indentation to improve legibility

--- refactoring.rb
+++ refactoring.rb
@@ -2,21 +2,22 @@ $VERBOSE = nil
 require 'prime'   
 
 def first_n_primes(n)
-unless n.is_a? Integer
-return "n must be an integer."
-end
 
-if n <= 0
-return "n must be greater than 0."
-end
+  unless n.is_a? Integer
+    return "n must be an integer."
+  end
+
+  if n <= 0
+    return "n must be greater than 0."
+  end
   
-prime_array = [] if prime_array.nil?
+  prime_array = [] if prime_array.nil?
   
-prime = Prime.new
-for num in (1..n)
-prime_array.push(prime.next)
-end
-return prime_array
+  prime = Prime.new
+  for num in (1..n)
+    prime_array.push(prime.next)
+  end
+  return prime_array
 end
 
 first_n_primes(10)
